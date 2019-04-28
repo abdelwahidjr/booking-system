375193fc98f5394e36d97fcc92589be0b45bc533
Abd El-Wahid JR <abdelwahid.jr@gmail.com>
2019-04-29 00:28:39 +0200

    changes

--- app/Http/Controllers/SinglePageController.php
+++ app/Http/Controllers/SinglePageController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Booking;
-use Faker\Factory;
 
 class SinglePageController extends Controller
 {
@@ -14,14 +12,9 @@ class SinglePageController extends Controller
 
     public function test()
     {
-        $faker = Factory::create();
 
-        $booking = Booking::create([
-            'start_at' => $faker->date(),
-            'end_at'   => $faker->date(),
-            'user_id'  => '1',
-            'room_id'  => rand(1,50),
-        ]);
+
+
 
     }
 
--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,8 @@
 <?php
 
 
-Broadcast::channel('App.User.{id}',function ($user,$id)
+Broadcast::channel('App.User.{id}',function (App\User $user,$id)
 {
     return (int)$user->id === (int)$id;
 });
+
